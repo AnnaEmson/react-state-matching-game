fbce4f0df774d8fef095e6df0c024f35d350a5bb
AnnaEmson <anna.emson@gmail.com>
2021-10-01 12:33:46 +0100

    indenation fixes

--- src/components/TileSelector/TileSelector.js
+++ src/components/TileSelector/TileSelector.js
@@ -5,13 +5,15 @@ import './TileSelector.css'
 const TileSelector = (props) => {
   const [ref, hovered] = useHover()
 
-  const dropdown = hovered ? (
-    <div className='tileSelectorContent'>
-      <div className='number' onClick={() => props.handleNumTileChange(4)}>4</div>
-      <div className='number' onClick={() => props.handleNumTileChange(16)}>16</div>
-      <div className='number' onClick={() => props.handleNumTileChange(36)}>36</div>
-    </div>
-  ) : null
+  const dropdown = hovered
+    ? (
+      <div className='tileSelectorContent'>
+        <div className='number' onClick={() => props.handleNumTileChange(4)}>4</div>
+        <div className='number' onClick={() => props.handleNumTileChange(16)}>16</div>
+        <div className='number' onClick={() => props.handleNumTileChange(36)}>36</div>
+      </div>
+      )
+    : null
 
   return (
     <div className='tileSelector'>
